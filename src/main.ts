8e92165a18a302e69a95859e3869704175e4a6ee
Tsironis Ioannis <gtsironis8@gmail.com>
2026-01-10 12:16:44 +0200

    add zoneless cd upgrade to angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [ ...appConfig.providers]})
   .catch((err) => console.error(err));
